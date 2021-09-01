7ccd3be74086a918dc35a4c0b37b896149fbe0b9
Slava <sh-e-95@mail.ru>
2021-09-01 23:03:55 +0500

    Create main Menu

--- ClickerApp/ViewModels/MainWindowViewModel.fs
+++ ClickerApp/ViewModels/MainWindowViewModel.fs
@@ -1,6 +1,14 @@
 ﻿namespace ViewModels
 
+open Avalonia
+
 type MainWindowViewModel() =
     inherit ViewModelBase()
 
+    member this.GetSize(appSize : Rect) = 
+        let height = appSize.Height/16.0*18.0/10.0
+        let weight = appSize.Width*0.9
+        new Rect(0.0,0.0,weight,height)
     member __.Greeting = "Hello World!"
+        
+        
--- ClickerApp/Views/MainWindow.xaml.fs
+++ ClickerApp/Views/MainWindow.xaml.fs
@@ -3,6 +3,7 @@
 open Avalonia
 open Avalonia.Controls
 open Avalonia.Markup.Xaml
+open Avalonia.Interactivity
 
 type MainWindow () as self = 
     inherit Window ()
@@ -10,3 +11,5 @@ type MainWindow () as self =
     do self.AttachDevTools()
     #endif
     do AvaloniaXamlLoader.Load self
+    member this.ExitApp(sender: obj , e: RoutedEventArgs) =
+        this.Close()
